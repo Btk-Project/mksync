9e39e5878cc262bd65d6524aa31632c896a53c1d
Snowinter <1944129869@qq.com>
2024-10-14 17:22:04 +0800

    add namespace define

--- src/Base/include/mksync/Base/osdep.h
+++ src/Base/include/mksync/Base/osdep.h
@@ -82,4 +82,9 @@
 #define mks_no_coroutine_fn
 #endif
 
+#define MKS_BEGIN                                                                                                              \
+    namespace mks                                                                                                              \
+    {
+#define MKS_END }
+
 #endif
