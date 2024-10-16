d8e06f2dad7794ba96f00d601d1b765191686a8d
Snowinter <1944129869@qq.com>
2024-10-16 08:56:02 +0800

    add argprase

--- src/Base/src/Environment.cpp
+++ src/Base/src/Environment.cpp
@@ -2,6 +2,7 @@
 
 #include <codecvt>
 #include <locale>
+#include <sstream>
 
 MKS_BEGIN
 MKS_BASE_BEGIN
@@ -20,6 +21,30 @@ char **convert_argc_argv(size_t argc, const wchar_t *const *wargv, std::string &
         argvector[i] = &args[pos[i]];
     }
     return argvector.data();
+}
+
+std::string string_to_argc_argv(const std::string &str, int &argc, std::vector<const char *> &argvector)
+{
+    std::istringstream       iss(str);
+    std::vector<std::string> args = {std::istream_iterator<std::string>{iss}, std::istream_iterator<std::string>{}};
+
+    argc = static_cast<int>(args.size());
+    argvector.resize(argc);
+
+    std::string buffer = {};
+    for (auto &arg : args) {
+        buffer.append(arg).append(1, '\0');
+    }
+
+    if (argc == 0) {
+        return buffer;
+    }
+
+    argvector[0] = buffer.data();
+    for (int i = 1; i < argc; ++i) {
+        argvector[i] = argvector[i - 1] + args[i - 1].size() + 1;
+    }
+    return buffer;
 }
 
 MKS_BASE_END
