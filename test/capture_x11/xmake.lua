ae2fecb482e3f715d5ce1e02abdffa4152c3195a
Snowinter <1944129869@qq.com>
2024-10-24 17:22:03 +0800

    add test

---
+++ test/capture_x11/xmake.lua
@@ -0,0 +1,22 @@
+
+
+if is_host("linux") and not is_plat("corss") then
+target("capture_x11")
+    set_kind("binary")
+    set_targetdir("$(testdir)")
+
+    add_deps("proto")
+    add_rules("targetclean")
+    add_packages("out_ptr", "sobjectizer", "spdlog")
+
+    -- version
+    -- set_configdir("./")
+    -- add_configfiles("*.rc.in")
+    -- add_files("*.rc")
+    -- target files
+    -- add_includedirs("include", {public = true})
+    -- add_headerfiles("include/(**)")
+    -- add_headerfiles("src/**.hpp", "src/**.h", {install = false})
+    add_files("src/**.cpp")
+target_end()
+end
