4d27c605d4f933b34c666496d0ec18d37027e01c
liuli-neko <llhsdmd@gmail.com>
2025-02-22 16:12:53 +0800

    [feature] split modules

---
+++ test/capture_win/src/command_parser.hpp
@@ -0,0 +1,52 @@
+/**
+ * @file command_parser.hpp
+ * @author llhsdmd (llhsdmd@gmail.com)
+ * @brief
+ * @version 0.1
+ * @date 2025-02-22
+ *
+ * @copyright Copyright (c) 2025
+ *
+ */
+#pragma once
+
+#include <string>
+#include <vector>
+#include <functional>
+#include <map>
+
+#include "trie.hpp"
+
+class App;
+
+class CommandParser {
+public:
+    using CommandsType = std::vector<std::string>;
+    using ArgsType     = std::vector<std::string>;
+    using OptionsType  = std::map<std::string, std::string>;
+    using CommandCallbackType =
+        std::function<std::string(const ArgsType &args, const OptionsType &options)>;
+
+    struct CommandsData {
+        CommandsType        command;
+        std::string         description;
+        CommandCallbackType callback;
+    };
+
+public:
+    CommandParser(App *app);
+    auto install_cmd(const CommandsData &command, std::string_view module = "") -> bool;
+    auto install_cmd(CommandsData &&command, std::string_view module = "") -> bool;
+    auto parser(std::string_view cmdline) -> std::string;
+    auto parser(std::vector<std::string_view> cmdline) -> std::string;
+    auto show_help() -> void;
+
+private:
+    static auto _split(std::string_view str, char ch = ' ') -> std::vector<std::string_view>;
+
+private:
+    std::unordered_multimap<std::string, int> _modules;
+    std::vector<CommandsData>                 _commands;
+    Trie<int>                                 _trie;
+    App                                      *_app = nullptr;
+};
